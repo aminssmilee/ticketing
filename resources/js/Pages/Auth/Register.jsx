dc1c3a78890b771ad17a9bdbf116ff508a811b97
aminssmilee <salisahmad48@gmail.com>
2025-12-01 19:58:50 +0700

    update

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,30 +1,6 @@
 "use client";
 
 import { RegisterForm } from "@/Components/regesister-form";
-<<<<<<< HEAD
-import registerImage from "/public/img/psn.jpg";
-
-export default function Register() {
-  return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-      
-      {/* LEFT IMAGE SECTION */}
-      <div className="hidden lg:flex items-center justify-center bg-[#0A1A2F]">
-        <img
-          src={registerImage}
-          alt="Register Illustration"
-          className="w-[80%] mx-auto drop-shadow-xl"
-        />
-      </div>
-
-      {/* RIGHT FORM SECTION */}
-      <div className="flex items-center justify-center p-10">
-        <div className="w-full max-w-md">
-          <RegisterForm />
-        </div>
-      </div>
-
-=======
 import registerImage from "/public/img/register.jpg";
 import logo from "/public/img/psn.jpg"; // ← tambahkan logo di sini
 
@@ -57,7 +33,6 @@ export default function Register() {
         </div>
       </div>
 
->>>>>>> 026c4634ba0ed3373bd84ef42d56b77ab9d66c13
     </div>
   );
 }
